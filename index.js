b648c3f2528df5f8b788c05aeab3f2cc85d19533
Laura <lnkeat@gmail.com>
2021-04-07 00:13:13 -0600

    Assignment Complete

--- index.js
+++ index.js
@@ -14,13 +14,29 @@ function logWhisper(string) {
     console.log(string.toLowerCase());
 }
 
+// function sayHiToGrandma(string) {
+//     if (string === string.toUpperCase()) {
+//         return "YES INDEED!";
+//     } else if (string === string.toLowerCase()) {
+//         return "I can't hear you!";
+//     } else if (string === "I love you, Grandma.") {
+//         return "I love you, too.";
+//     }
+// }
+
 function sayHiToGrandma(string) {
-    if (string === string.toUpperCase()) {
-        return "YES INDEED!";
-    } else if (string === string.toLowerCase()) {
-        return "I can't hear you!";
-    } else if (string === "I love you, Grandma.") {
-        return "I love you, too.";
+    let response;
+    switch (string) {
+        case string.toUpperCase():
+            response = "YES INDEED!";
+            break;
+        case string.toLowerCase():
+            response = "I can't hear you!"
+            break;
+        case "I love you, Grandma.":
+            response = "I love you, too."
     }
+    return response
 }
+
 
